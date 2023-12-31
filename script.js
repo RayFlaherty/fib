9c49748e0314d2a31d8f9a6d9d2a6a3707931f18
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2023-12-30 19:41:59 -0600

    fib seq fixgit status!

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const btnGo = document.getElementById("go");
 btnGo.onclick = myClickHandler;
 
 function myClickHandler() {
-  const intX = document.getElementById("x").value;
-  console.log(intX);
-  for (var i = intX + 1; i <= 20; i++) {
-    console.log(i);
+  const intX = Number(document.getElementById("x").value);
+  let n1 = 0,
+    n2 = 1,
+    nextTerm;
+  for (var num = n1 + n2; num <= intX; num++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+    document.write(num + ". " + nextTerm);
+    document.write("</br>");
   }
 }
